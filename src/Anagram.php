cee9103847886d4d4d7316f04edd8cf76279da67
nic netzel and torrence stratton <netzeln@gmail.com>
2016-02-16 09:30:59 -0800

    pass first spec test for comparing single word

--- src/Anagram.php
+++ src/Anagram.php
@@ -1,9 +1,26 @@
 <?php
 class Anagram {
-  function MakeAnagram($input_word, $input_list)
-  {
 
-  }
+      function makeAnagram($input_word, $comparisons)
+      {
+          $word_to_compare  = str_split($input_word);
+          $comparingword = str_split($comparisons);
+
+          if ($word_to_compare == $comparingword){
+              return $comparisons;
+          }
+        //   $sorted_word_compare = sort($word_to_compare, SORT_FLAG_CASE);
+        //   $comparisons_split = array();
+          //
+        //   foreach($comparisons as $comparison)
+        //   {
+        //       $split_comparison = str_split($comparison);
+        //       $sorted_split = sort($split_comparison, SORT_FLAG_CASE);
+        //       array_push($comparisons_split, $sorted_split);
+        //   }
 
 
-} ?>
+      }
+}
+
+?>
---
+++ tests/AnagramTest.php
@@ -0,0 +1,50 @@
+<?php
+  require_once "src/Anagram.php";
+
+  class AnagramTest extends PHPUnit_Framework_TestCase
+  {
+      function test_makeAnagram_singlesameword()
+      {
+          //Arrange
+          $test_Anagram = new Anagram;
+          $input = "nap";
+          $comparisons ="nap";
+
+          //Act
+          $result = $test_Anagram->makeAnagram($input, $comparisons);
+
+          //Assert
+          $this->assertEquals("nap", $result);
+      }
+
+      function test_makeAnagram_singleDifword()
+      {
+          //Arrange
+          $test_Anagram = new Anagram;
+          $input = "nap";
+          $comparisons ="pan";
+
+          //Act
+          $result = $test_Anagram->makeAnagram($input, $comparisons);
+
+          //Assert
+          $this->assertEquals("pan", $result);
+      }
+  }
+
+
+  // function test_makeAnagram_array()
+  // {
+  //     //Arrange
+  //     $test_Anagram = new Anagram;
+  //     $input = "nap";
+  //     $comparisons = array("nap");
+  //
+  //     //Act
+  //     $result = $test_Anagram->makeAnagram($input, $comparisons);
+  //
+  //     //Assert
+  //     $this->assertEquals("nap", $result);
+  // }
+  // }
+ ?>
